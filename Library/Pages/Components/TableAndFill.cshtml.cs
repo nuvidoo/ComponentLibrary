5c3219a19868c845f62adf1eb7e542f8b6b36e3b
İsmet Buğra Özler <i.bugraozler@gmail.com>
2024-01-11 14:53:42 +0300

    Table component has been added

---
+++ Library/Pages/Components/TableAndFill.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Library.Pages.Components
+{
+    public class TableAndFillModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
